1ea583e3c57181a565ebaa61a9e05ba0ed033326
Mithgal_JH <mithgaljamal@gmail.com>
2026-01-19 01:36:07 +0200

    connect and test databse

--- blog/database.py
+++ blog/database.py
@@ -1,6 +1,13 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE = "sqlite:/// ./blog.db"
+
+SQLALCHAMY_DATABASE = "sqlite:///./blog.db"
 connect_args = {"check_same_thread": False}
 
 engine = create_engine(SQLALCHAMY_DATABASE, connect_args=connect_args)
+
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+base = declarative_base()
